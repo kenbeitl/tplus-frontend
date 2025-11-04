46f56b295c7cf2d013daff9b8931fe8a39d94ef8
Ken Bei <KenBei@tl.com>
2025-11-04 18:37:29 +0800

    rollback to before edit pay-connect

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // Temporarily disable type checking during build
-    ignoreBuildErrors: false,
-  },
-  typedRoutes: false,
   /* config options here */
 };
 
